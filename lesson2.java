aef0690de3a73a0e34d54bdc314665707df3273a
ARNOLD ACHIKI <achikiarnold@gmail.com>
2025-02-20 10:59:21 +0300

    Initial commit

---
+++ lesson2.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+/*
+ * dialog boxes
+************
+JOptionPane class found in java.string package
+which methods in that class are we interested in
+
+
+
+ */
+public class lesson2 {
+    
+}
