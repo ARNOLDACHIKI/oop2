5f99be8492898080087b925922a25efbb9a2f34d
ARNOLDACHIKI <142616073+ARNOLDACHIKI@users.noreply.github.com>
2025-04-18 21:55:57 +0300

    Add comment to clarify purpose of Math class as a JFrame

--- BCSDemo5/Login.java
+++ BCSDemo5/Login.java
@@ -1,8 +1,9 @@
-
-import java.awt.FlowLayout;
+import java.awt.FlowLayout; //interface
+import java.awt.event.ActionEvent;//class
+import java.awt.event.ActionListener;
 import javax.swing .* ;
 
-public class  Login extends JFrame{
+public class  Login extends JFrame implements ActionListener{
     private JLabel lblUser,lblpword;
     private JTextField txtUser=new JTextField(20);
     private JPasswordField pwfpass;
@@ -11,14 +12,25 @@ public class  Login extends JFrame{
         super("Login Form");
         
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-       setLayout(new FlowLayout());
+        setLayout(new FlowLayout());
        // setLayout(newGridLayout(4,2));
         lblUser = new JLabel("UserName");
         lblpword = new JLabel("Password");
-        pwfpass = new JPasswordField(15);
+        pwfpass = new JPasswordField(20);
         btnLogin=new JButton("Login");
-        btnClear=new JButton("Clear ");
+        btnLogin.addActionListener(new Handler());
+        
+        btnClear=new JButton("Reset ");
+         //anonymous-event handler
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                txtUser.setText("");
+                pwfpass.setText("");
+            }
+        });
         btnExit=new JButton("Exit program");
+        btnExit.addActionListener(this);
         setVisible(true);
         setSize(300,200);
         add(lblUser);
@@ -29,9 +41,45 @@ public class  Login extends JFrame{
         add(btnClear);
         add(btnExit);
         
+       
+    }
+   
+
+    //Action Listener  event listener
+    private class Handler implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            String user=txtUser.getText();
+            String pword=pwfpass.getText();
+            //equals()-compares two strings including their case--used fr case sensitivity
+            //equalsIgnoreCase();-compare strings but ignore their case-used when you wanna relax the case
+            //username=student
+            //password=BCS2.2
+            if (user.equalsIgnoreCase("lod") && pword.equals("1234")){
+                JOptionPane.showMessageDialog(null,"Welcome to Main Form");
+                new Math();
+                setVisible(false);
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Authetication failed \n Try again");
+                pwfpass.setText("");//resets the password fiels
+            }
+
+        }
 
     }
+    
+
+
     public static void main(String [] S){
         new Login();
     }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        
+        int x=JOptionPane.showConfirmDialog(this,"Are you sure you want to exit f");
+        if(x==0){
+            System.exit(0);
+        
+    }}
 }
--- BCSDemo5/Math.java
+++ BCSDemo5/Math.java
@@ -1,21 +1,28 @@
 // Removed package declaration
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
-import javax.swing.JTextField;
-
+import javax.swing.JTextField;//class
 
 public class Math  extends JFrame{   /// the function  of this code is to make it a window with a title bar and  a boarder
+    
     JLabel lbl1,lbl2,lblResult,lblOperation,lblGender;
     JLabel lblSelectList=new JLabel("Select List");
     JTextField txt1,txt2,txtresult;
     JRadioButton rbtnAdd=new JRadioButton("Add");
+    
+
     JRadioButton rbtnProduct=new JRadioButton("product");
+    
+    
     JRadioButton rbtnDiff=new JRadioButton("Difference");
+    
+
     JRadioButton rbtnFemale=new JRadioButton("Female");
     JRadioButton rbtnMale=new JRadioButton("Male");
     JButton btnSysLock,btnExit;
@@ -26,16 +33,16 @@ public class Math  extends JFrame{   /// the function  of this code is to make i
     public Math() {
         super("Math Operation");
         setLayout(new FlowLayout()); // Use FlowLayout for simplicity
-
+        setSize(300,200);
         // Initialize components
         lbl1 = new JLabel("Input 1");
         lbl2 = new JLabel("Input 2");
         lblGender = new JLabel("Gender");
         lblResult = new JLabel("Result");
         lblOperation = new JLabel("Operation");
-        txt1 = new JTextField(15);
-        txt2 = new JTextField(15);
-        txtresult = new JTextField(15);
+        txt1 = new JTextField(20);
+        txt2 = new JTextField(20);
+        txtresult = new JTextField(20);
         txtresult.setEditable(false);
         btnSysLock = new JButton("System Lock");
         btnExit = new JButton("Exit Program");
@@ -67,13 +74,34 @@ public class Math  extends JFrame{   /// the function  of this code is to make i
         bgOperation.add(rbtnDiff);
         bgGender.add(rbtnFemale);
         bgGender.add(rbtnMale);
+        rbtnAdd.addActionListener(new ActionListener());
+        rbtnProduct.addActionListener(new ActionListener());
+        rbtnDiff.addActionListener((ActionListener)this);
 
         // Adjust frame size and make it visible
-        pack(); // Automatically sizes the frame to fit components
+        //pack(); // Automatically sizes the frame to fit components
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true); // Set visibility after packing
         
+        
     }
+        public void actionPerformed(ActionEvent e){
+            int val1=Integer.parseInt(txt1.getText());
+            int val2=Integer.parseInt(txt1.getText());
+            int result;
+            //getSource-returns the name of the object eg rbtnAdd
+            //getActionCommand-returns the caption eg sum 
+            if (e.getActionCommand().equals("sum")){
+                result=val1+val2;
+                txtresult.setText(Integer.toString(result));
+
+            }
+            if(e.getSource()==rbtnProduct){
+                result=val1*val2;
+                txtresult.setText(Integer.toString(result));
+            }
+            
+        }
     
     public static void main(String[] S) {
         new Math();
